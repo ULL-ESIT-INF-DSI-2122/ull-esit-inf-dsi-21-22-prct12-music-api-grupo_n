8bfcb175aea0b007cb12cd136a8eefe13504641f
usuario <alu0101100586@ull.edu.es>
2022-05-16 19:11:55 +0000

    First step

---
+++ src/server/server.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import './db/mongoose';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/notes', (req, res) => {
+  const note = new Note(req.body);
+
+  // llamar a una funcion addsong por ejemplo
+  note.save().then((note) => {
+    res.send(note);
+  }).catch((error) => {
+    res.send(error);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
